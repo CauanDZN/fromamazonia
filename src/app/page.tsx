2874a0a3495a35c9724d1f6ff6d4a12003672bcc
Cauan Victor <cauanvictor0325@gmail.com>
2023-05-19 18:51:32 -0300

    feat: initial commit :tada:

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+  function getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const [treesCut, setTreesCut] = useState(72130714);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTreesCut(prevCount => prevCount + getRandomNumber(10, 50));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Contador de Árvores Cortadas</h1>
+      <p className="text-4xl font-bold transition-opacity animate-fade-in">
+        {treesCut}
+      </p>
+    </div>
+  );
+}
