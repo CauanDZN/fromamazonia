41981fda659ca6d59a2feac8c28cd95c587b94f5
Cauan Victor <cauanvictor0325@gmail.com>
2023-05-20 15:01:22 -0300

    feat: adding header and sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,155 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import Image from 'next/image'
+import Header from '@/components/Header'
+import amazonia from '../../public/amazonia.jpeg'
+import analista from '../../public/analista.png'
+import programador from '../../public/programador.png'
+import qualidade from '../../public/qualidade.png'
+import { At, InstagramLogo, MapPin, WhatsappLogo } from '@phosphor-icons/react'
+import { Suspense } from 'react'
+import Loading from './loading'
+import { Counter } from '@/components/Counter'
 
 export default function Home() {
-  function getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  const [treesCut, setTreesCut] = useState(72130714);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTreesCut(prevCount => prevCount + getRandomNumber(10, 50));
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div>
-      <h1>Contador de Árvores Cortadas</h1>
-      <p className="text-4xl font-bold transition-opacity animate-fade-in">
-        {treesCut}
-      </p>
-    </div>
-  );
+    <Suspense fallback={<Loading />}>
+      <Header />
+      <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4'>
+        <h1 className='text-white text-center'>Somos uma equipe da EEEP Leonel de Moura Brizola. 🌳</h1>
+        <div className="flex justify-center items-center">
+            
+        </div>
+      </div>
+
+      <div className='items-center justify-center flex flex-wrap bg-white w-full gap-2'>
+        <div className="flex justify-around items-center gap-4 p-6 max-w-6xl 2xl:flex-row flex-col">
+          <div className={`flex flex-col sm:flex-row 2xl:flex-col gap-2 my-2 sm:w-auto`}>
+            <Counter />
+          </div>
+          <div className={`flex flex-col sm:flex-row 2xl:flex-col gap-2 my-4 w-full sm:w-auto`}>
+            <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full'>Saiba Mais</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4 gap-2'>
+        <div className="flex justify-between items-center p-12 w-full max-w-6xl 2xl:flex-row flex-col">
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-4xl font-medium text-white'>Lorem ipsum</h1>
+            <p className='text-sm font-normal text-white'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className='w-full flex'>
+              <Image src={amazonia} alt='Nota 5' width={500} className='border' />
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='items-center justify-center flex flex-wrap bg-white-600 w-full p-4 gap-2'>
+        <div className="flex justify-between items-center p-12 gap-8 w-full max-w-6xl 2xl:flex-row-reverse flex-col">
+          <div className='flex flex-col gap-8'>
+            <div>
+              <h1 className='text-4xl font-medium text-gray-900'>Lorem ipsum</h1>
+              <p className='text-sm font-normal text-gray-900'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+
+            <div className='flex items-center gap-8 flex-col sm:flex-row justify-around sm:justify-start'>
+              <Image src={analista} alt='Rocket' width={300} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4 gap-2'>
+        <div className="flex justify-between items-center p-12 w-full max-w-6xl 2xl:flex-row flex-col">
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-4xl font-medium text-white'>Lorem ipsum</h1>
+            <p className='text-sm font-normal text-white'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className='w-full flex'>
+              <Image src={programador} alt='Nota 5' width={500} className='border' />
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='items-center justify-center flex flex-wrap bg-white-600 w-full p-4 gap-2'>
+        <div className="flex justify-between items-center p-12 gap-8 w-full max-w-6xl 2xl:flex-row-reverse flex-col">
+          <div className='flex flex-col gap-8'>
+            <div>
+              <h1 className='text-4xl font-medium text-gray-900'>Lorem ipsum</h1>
+              <p className='text-sm font-normal text-gray-900'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+
+            <div className='flex items-center gap-8 flex-col sm:flex-row justify-around sm:justify-start'>
+              <Image src={qualidade} alt='Rocket' width={600} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4'>
+      <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
+        <div className="px-4 py-6">
+          <div className="mb-2 text-xl text-white font-bold">Newsletter</div>
+          <form className="flex flex-col">
+            <label htmlFor="name" className="mb-2 text-white">Nome</label>
+            <input
+              className="mb-4 border-b-2 p-4"
+              id="name"
+              name="name"
+              type="text"
+              required
+            />
+            <label htmlFor="email" className="mb-2 text-white">E-mail</label>
+            <input
+              className="mb-4 border-b-2 p-4"
+              id="email"
+              name="email"
+              type="email"
+              required
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 font-bold text-white bg-green-500 rounded-full hover:bg-green-700"
+            >
+              Participar
+            </button>
+          </form>
+        </div>
+      </div>
+      </form>
+
+      <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4 gap-2'>
+        <div className='flex flex-col p-12 w-full max-w-6xl gap-8'>
+          <div>
+            <h1 className='text-4xl font-medium text-white'>Contatos</h1>
+            <p className='text-sm font-normal text-white'>
+              Transforme! Ajude!
+            </p>
+          </div>
+
+          <div className='flex flex-col 2xl:flex-row gap-6 items-center sm:items-start'>
+            <div className='flex flex-row gap-2 items-center'>
+              <InstagramLogo size={24} color='#FFFFFF' weight='fill' />
+              <p className='text-sm text-white'>@fromamazonia</p>
+            </div>
+            <div className='flex flex-row gap-2 items-center'>
+              <At size={24} color='#FFFFFF' weight='regular' />
+              <p className='text-sm text-white'>fromamazonia@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import fromamazonia from '../../public/fromamazonia.png'
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <header className="flex justify-center items-center px-4 py-2 bg-gray-100 w-full">
+      <div className="flex justify-between items-center w-full max-w-6xl">
+        <div className="flex items-center p-4">
+          <a href="/">
+            <Image src={fromamazonia} alt="Logo" width={225} />
+          </a>
+        </div>
+
+        <nav className='hidden lg:block'>
+          <ul className="flex">
+            <li className="mx-2">
+              <a href="">Instagram</a>
+            </li>
+            <li className="mx-2">
+              <a href="">Linktree</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="lg:hidden">
+          <button
+            className="text-gray-600 hover:text-gray-900 focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <FaBars />
+          </button>
+        </div>
+        
+        
+
+        <nav
+          className={`${isMenuOpen ? 'flex' : 'hidden'} bg-gray-300 flex-1 p-2 gap-1 lg:hidden absolute top-24 right-0 h-[calc(100%-6rem)] w-full justify-center`}
+        >
+          <ul className="flex flex-col text-center">
+            <li className="mx-2 p-2 hover:underline">
+              <a href="">Instagram</a>
+            </li>
+            <li className="mx-2 p-2 hover:underline">
+              <a href="">Linktree</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
