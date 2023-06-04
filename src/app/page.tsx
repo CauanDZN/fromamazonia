de4cc12f47cf09c99ec291b89c585b0f22b74800
Cauan Victor <cauanvictor0325@gmail.com>
2023-06-04 20:46:10 -0300

    feat: adding text and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@
 import Image from 'next/image'
 import Header from '@/components/Header'
 import amazonia from '../../public/amazonia.jpeg'
-import analista from '../../public/analista.png'
+import desmatamento from '../../public/desmatamento.jpeg'
 import programador from '../../public/programador.png'
-import qualidade from '../../public/qualidade.png'
+import oceanos from '../../public/oceanos.jpg'
 import { At, InstagramLogo, MapPin, WhatsappLogo } from '@phosphor-icons/react'
 import { Suspense } from 'react'
 import Loading from './loading'
@@ -36,9 +36,15 @@ export default function Home() {
       <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4 gap-2'>
         <div className="flex justify-between items-center p-12 w-full max-w-6xl 2xl:flex-row flex-col">
           <div className='flex flex-col gap-4'>
-            <h1 className='text-4xl font-medium text-white'>Lorem ipsum</h1>
+            <h1 className='text-4xl font-medium text-white'>Conhecendo a Amazônia</h1>
             <p className='text-sm font-normal text-white'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Sendo uma das maiores florestas tropicais, localiza-se no norte da América do Sul
+            cobrindo regiões não só brasileiras,mas também países como: Peru, Bolívia,
+            Venezuela, Colômbia e outros. Possui um clima equatorial e solo seco. Sua fauna e
+            flora são de extrema importância, visto que a floresta Amazônica tem uma vasta
+            biodiversidade. Sua conservação é necessária devido aos efeitos positivos sobre
+            recursos naturais encontrados na sua área, além de contribuir para o equilíbrio
+            climático do mundo.
             </p>
             <div className='w-full flex'>
               <Image src={amazonia} alt='Nota 5' width={500} className='border' />
@@ -48,18 +54,25 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='items-center justify-center flex flex-wrap bg-white-600 w-full p-4 gap-2'>
+      <div className='items-center justify-center flex flex-wrap bg-gray-900 w-full p-4 gap-2'>
         <div className="flex justify-between items-center p-12 gap-8 w-full max-w-6xl 2xl:flex-row-reverse flex-col">
           <div className='flex flex-col gap-8'>
             <div>
-              <h1 className='text-4xl font-medium text-gray-900'>Lorem ipsum</h1>
-              <p className='text-sm font-normal text-gray-900'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <h1 className='text-4xl font-medium text-white'>Uma de suas ameaças; o desmatamento</h1>
+              <p className='text-sm font-normal text-white'>
+              O desmatamento ocorre devido o desflorestamento de um determinado local, ou
+              seja, a derrubada de árvores. No caso da floresta Amazônica, o desmatamento
+              ocorre em decorrência da exploração de recursos encontrados na floresta, muitas
+              vezes esse desmatamento ocorre de forma ilegal, sendo assim, não possui
+              nenhuma autorização de órgãos responsáveis. Suas causas princípais então entre a
+              exploração florestal, o agronégocio, o garimpo ilegal e a grilagem e seus impactos
+              negativos são diversos para o meio ambiente como o aumento de gases do efeito
+              estufa, mudanças climáticas, além da perda da biodiversidade.
               </p>
             </div>
 
             <div className='flex items-center gap-8 flex-col sm:flex-row justify-around sm:justify-start'>
-              <Image src={analista} alt='Rocket' width={300} />
+              <Image src={desmatamento} alt='Rocket' width={300} />
             </div>
           </div>
         </div>
@@ -68,14 +81,26 @@ export default function Home() {
       <div className='items-center justify-center flex flex-wrap bg-green-600 w-full p-4 gap-2'>
         <div className="flex justify-between items-center p-12 w-full max-w-6xl 2xl:flex-row flex-col">
           <div className='flex flex-col gap-4'>
-            <h1 className='text-4xl font-medium text-white'>Lorem ipsum</h1>
+            <h1 className='text-4xl font-medium text-white'>Como podemos contribuir para a conservação da floresta amazônica</h1>
             <p className='text-sm font-normal text-white'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            A conscientização ambiental de qualquer pessoa pode ajudar na preservação da
+            floresta Amazônica, tomando para si atitudes que não gerem consequências para o
+            coletivo, mas sim atitudes positivas que visem o futuro mais sustentável para futuras
+            gerações. Atitudes como;
+            O consumo consciente: Ter conhecimento da fabricação do produto, dando
+            preferência para matérias que sejam frutos de um processo livre de impactos
+            negativos para o meio ambiente.
+            Contribuições com ONGs: Muitas ongs não tem um apoio governamental,
+            sendo assim, é importante ter conhecimento sobre essas ongs que precisam
+            de ajuda para seu funcionamento. Ajudando através de doações para
+            contribuir com as atividades do grupo.
+            Apoiando as lutas dos povos tradicionais: Entender e valorizar o
+            conhecimento dos povos e comunidades tradicionais, como da população
+            indígena. Apoiando lutas e ações promovidas pelos povos tradicionais.
             </p>
             <div className='w-full flex'>
               <Image src={programador} alt='Nota 5' width={500} className='border' />
             </div>
-
           </div>
         </div>
       </div>
@@ -84,14 +109,19 @@ export default function Home() {
         <div className="flex justify-between items-center p-12 gap-8 w-full max-w-6xl 2xl:flex-row-reverse flex-col">
           <div className='flex flex-col gap-8'>
             <div>
-              <h1 className='text-4xl font-medium text-gray-900'>Lorem ipsum</h1>
+              <h1 className='text-4xl font-medium text-gray-900'>Curiosidade!</h1>
               <p className='text-sm font-normal text-gray-900'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Muitos dizem que a floresta Amazônia é o pulmão do mundo, mas na verdade o real
+              pulmão do mundo são os oceanos, devido às algas marinhas. Visto que as algas
+              produzem mais oxigênio para a fotossíntese do que precisam, e desse excesso
+              ocorre a liberação para o ambiente. Mas ainda assim, a floresta Amazônica é
+              essencial para o equilíbrio climático, evitando assim, possíveis danos irreversíveis
+              para o meio ambiente.
               </p>
             </div>
 
             <div className='flex items-center gap-8 flex-col sm:flex-row justify-around sm:justify-start'>
-              <Image src={qualidade} alt='Rocket' width={600} />
+              <Image src={oceanos} alt='Rocket' width={400} />
             </div>
           </div>
         </div>
